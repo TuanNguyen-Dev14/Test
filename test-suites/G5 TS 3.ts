2bd533ec153a2a1cbd0718ef76a67e602bf86977
TuanNguyen-Dev14 <79506582+TuanNguyen-Dev14@users.noreply.github.com>
2023-04-03 14:57:58 +0700

    test

---
+++ test-suites/G5 TS 3.ts
@@ -0,0 +1,9 @@
+<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
+<TestSuiteEntity>
+    <name>G5 TS 3</name>
+    <testSuiteGuid>d3d7efbb-41df-4a97-ac65-23016c711901</testSuiteGuid>
+    <testCaseLink>
+        <testCaseId>test-cases/G5 Test Case</testCaseId>
+        <guid>071512c4-7d98-4bb6-83ad-b62a715bd14d</guid>
+    </testCaseLink>
+</TestSuiteEntity>
