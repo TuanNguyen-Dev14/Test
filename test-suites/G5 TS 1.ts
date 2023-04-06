1ee5739134a6b956af8889a4087ce8e42ca0112c
TuanNguyen-Dev14 <79506582+TuanNguyen-Dev14@users.noreply.github.com>
2023-04-06 10:07:42 +0700

    tuan

--- test-suites/G5 TS 1.ts
+++ test-suites/G5 TS 1.ts
@@ -1,9 +1,17 @@
 <?xml version="1.0" encoding="UTF-8" standalone="yes"?>
 <TestSuiteEntity>
     <name>G5 TS 1</name>
-    <testSuiteGuid>bf472610-9dd6-4ee1-9618-b723a26ec130</testSuiteGuid>
+    <testSuiteGuid>f44cf021-4a3c-4fc3-9b67-9dbac44de0cd</testSuiteGuid>
     <testCaseLink>
         <testCaseId>test-cases/G5 Test Case</testCaseId>
-        <guid>1016a0ed-dcd0-4955-9e4c-c90a4af75994</guid>
+        <guid>29aa5353-c585-4b7c-a592-4a38015512b8</guid>
+    </testCaseLink>
+    <testCaseLink>
+        <testCaseId>test-cases/G5 Test Case</testCaseId>
+        <guid>4c4ee264-18a4-4e1c-a0bc-d9ae3bc100bf</guid>
+    </testCaseLink>
+    <testCaseLink>
+        <testCaseId>test-cases/G5 Test Case 2</testCaseId>
+        <guid>3e2597cd-8572-4a5b-9e46-5a59e59c2156</guid>
     </testCaseLink>
 </TestSuiteEntity>
