2e2345f65ebef8266358c7fabf016654b3ff4a13
TuanNguyen-Dev14 <79506582+TuanNguyen-Dev14@users.noreply.github.com>
2023-04-06 10:09:11 +0700

    Katalon: Update test suite G5 TS 1 in test-suites

--- test-suites/G5 TS 1.ts
+++ test-suites/G5 TS 1.ts
@@ -1,9 +1,9 @@
 <?xml version="1.0" encoding="UTF-8" standalone="yes"?>
 <TestSuiteEntity>
     <name>G5 TS 1</name>
-    <testSuiteGuid>eaabd5ab-97cb-40db-98a0-6e338d2fd308</testSuiteGuid>
+    <testSuiteGuid>e839f023-4588-4336-b938-6a64ad3be9bf</testSuiteGuid>
     <testCaseLink>
         <testCaseId>test-cases/G5 Test Case 2</testCaseId>
-        <guid>2f748a7d-6f0e-4cd4-a01a-a5726c6b80e5</guid>
+        <guid>35d05fff-081d-4eb4-b2af-129c86a3fecd</guid>
     </testCaseLink>
 </TestSuiteEntity>
