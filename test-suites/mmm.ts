fc113364a304a2c66b6ceff61c8f58ba05f5b006
TuanNguyen-Dev14 <79506582+TuanNguyen-Dev14@users.noreply.github.com>
2023-07-04 10:15:04 +0700

    m

---
+++ test-suites/mmm.ts
@@ -0,0 +1,9 @@
+<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
+<TestSuiteEntity>
+    <name>mmm</name>
+    <testSuiteGuid>8f8cb122-1fb1-486c-a80f-59ca046c56f9</testSuiteGuid>
+    <testCaseLink>
+        <testCaseId>test-cases/TC1</testCaseId>
+        <guid>6bebbc18-26d9-4f94-9be0-df4276a8a642</guid>
+    </testCaseLink>
+</TestSuiteEntity>
