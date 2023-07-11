c7d24424c7c63f05f4b1b163dbfd7ce88b634fdf
TuanNguyen-Dev14 <79506582+TuanNguyen-Dev14@users.noreply.github.com>
2023-07-11 15:55:03 +0700

    m

---
+++ test-suites/m.ts
@@ -0,0 +1,9 @@
+<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
+<TestSuiteEntity>
+    <name>m</name>
+    <testSuiteGuid>1589e6b4-c1cd-41f2-8364-fd339744f4fb</testSuiteGuid>
+    <testCaseLink>
+        <testCaseId>test-cases/TC1</testCaseId>
+        <guid>5e4130ab-1150-4e1d-be27-e720da08a279</guid>
+    </testCaseLink>
+</TestSuiteEntity>
