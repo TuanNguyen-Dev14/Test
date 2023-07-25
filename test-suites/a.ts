7a1c5125fa457c1aeb06c88501e655f9beac83ec
TuanNguyen-Dev14 <79506582+TuanNguyen-Dev14@users.noreply.github.com>
2023-07-25 15:49:12 +0700

    mmmoo

--- test-suites/a.ts
+++ test-suites/a.ts
@@ -1,9 +1,9 @@
 <?xml version="1.0" encoding="UTF-8" standalone="yes"?>
 <TestSuiteEntity>
     <name>a</name>
-    <testSuiteGuid>3361d4c1-3711-44aa-8194-6358ea591f09</testSuiteGuid>
+    <testSuiteGuid>cef47378-5a05-493d-bd1e-9f2c6c1d0de3</testSuiteGuid>
     <testCaseLink>
         <testCaseId>test-cases/TC1</testCaseId>
-        <guid>fe06b3b5-2b9e-48e4-9d31-e0d488b3d4d9</guid>
+        <guid>fcdc1f86-59f8-4d44-97e5-2c100681bacb</guid>
     </testCaseLink>
 </TestSuiteEntity>
