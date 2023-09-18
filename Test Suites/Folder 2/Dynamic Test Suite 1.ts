e11a75bbd97b35b175f43c481be42b3afd83bd50
TuanNguyen-Dev14 <tuan.nguyenhuu.106902@gmail.com>
2023-09-18 16:13:47 +0700

    a

---
+++ Test Suites/Folder 2/Dynamic Test Suite 1.ts
@@ -0,0 +1,15 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<FilteringTestSuiteEntity>
+   <description></description>
+   <name>Dynamic Test Suite 1</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>def58a58-cf06-407d-8d60-763f9a11ff22</testSuiteGuid>
+   <filteringText></filteringText>
+</FilteringTestSuiteEntity>
---
+++ Test Suites/Folder 2/Dynamic Test Suite 2.ts
@@ -0,0 +1,15 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<FilteringTestSuiteEntity>
+   <description></description>
+   <name>Dynamic Test Suite 2</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>2b53c8a4-62e1-4d7a-b35f-e5845a6e45d7</testSuiteGuid>
+   <filteringText></filteringText>
+</FilteringTestSuiteEntity>
