bb62b0ba935107a62ee1962633c9fa0e0801d262
TuanNguyen-Dev14 <tuan.nguyenhuu.106902@gmail.com>
2023-09-25 11:45:03 +0700

    a

---
+++ Test Suites/Folder 1/TestOps/Test Suite 6.ts
@@ -0,0 +1,51 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Test Suite 6</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>7e835799-4b11-4182-96c2-58013430bc35</testSuiteGuid>
+   <testCaseLink>
+      <guid>16156b4f-c2fd-417c-b19f-bc0798e9509d</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TC1</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+      <variableLink>
+         <testDataLinkId></testDataLinkId>
+         <type>DEFAULT</type>
+         <value></value>
+         <variableId>e2b706c3-2666-455f-b957-bbabc58ee31d</variableId>
+      </variableLink>
+      <variableLink>
+         <testDataLinkId></testDataLinkId>
+         <type>DEFAULT</type>
+         <value></value>
+         <variableId>5823e495-d627-4e97-8f54-15db3dc1be09</variableId>
+      </variableLink>
+      <variableLink>
+         <testDataLinkId></testDataLinkId>
+         <type>DEFAULT</type>
+         <value></value>
+         <variableId>9e3d6651-917d-43b9-83f3-d98375526b75</variableId>
+      </variableLink>
+      <variableLink>
+         <testDataLinkId></testDataLinkId>
+         <type>DEFAULT</type>
+         <value></value>
+         <variableId>1af93a4a-cb31-426c-8d03-95abbee1cdca</variableId>
+      </variableLink>
+      <variableLink>
+         <testDataLinkId></testDataLinkId>
+         <type>DEFAULT</type>
+         <value></value>
+         <variableId>93976fab-28b0-4cb1-b209-61eba0e3ece3</variableId>
+      </variableLink>
+   </testCaseLink>
+</TestSuiteEntity>
