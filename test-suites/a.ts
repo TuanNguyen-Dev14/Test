5b89085ec324bcf4d4b3b27c2a332d4939b4b85d
TuanNguyen-Dev14 <79506582+TuanNguyen-Dev14@users.noreply.github.com>
2023-10-12 10:11:54 +0700

    a

--- test-suites/a.ts
+++ test-suites/a.ts
@@ -1,9 +1,9 @@
 <?xml version="1.0" encoding="UTF-8" standalone="yes"?>
 <TestSuiteEntity>
     <name>a</name>
-    <testSuiteGuid>cef47378-5a05-493d-bd1e-9f2c6c1d0de3</testSuiteGuid>
+    <testSuiteGuid>a53431dd-b067-4acc-b90a-e70391c19f94</testSuiteGuid>
     <testCaseLink>
         <testCaseId>test-cases/TC1</testCaseId>
-        <guid>fcdc1f86-59f8-4d44-97e5-2c100681bacb</guid>
+        <guid>981f7390-ca99-47fc-99c2-467e0e344daf</guid>
     </testCaseLink>
 </TestSuiteEntity>
